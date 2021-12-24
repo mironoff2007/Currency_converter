66fbf066c1a797cbfddf6f4227d9affbabd37427
mironoff2007 <mironoff2007@rambler.ru>
2021-12-24 12:57:53 +0300

    CurrencyFragment navigation is added

--- app/src/main/java/ru/mironov/currencyconverter/TabsFragment.kt
+++ app/src/main/java/ru/mironov/currencyconverter/TabsFragment.kt
@@ -2,13 +2,30 @@ package ru.mironov.currencyconverter
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import ru.mironov.currencyconverter.databinding.TabsFragmentBinding
 
 class TabsFragment:Fragment(R.layout.tabs_fragment) {
+
+    private lateinit var binding: TabsFragmentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requireContext().appComponent.inject(this)
         Log.d("My_tag",this.toString())
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = TabsFragmentBinding.bind(view)
+
+        val navHost = childFragmentManager.findFragmentById(R.id.tabsContainer) as NavHostFragment
+        val navController = navHost.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+    }
+
 }
